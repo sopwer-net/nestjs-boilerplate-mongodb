f3e31e2506dd054225425eab7ce042d847a9b256
budazimbud <budazimbud@gmail.com>
2022-01-18 15:04:15 +0700

    file manager

--- src/core/file-manager/file-manager-google.service.ts
+++ src/core/file-manager/file-manager-google.service.ts
@@ -1,19 +1,38 @@
-import { bucketGoogle, expires, options } from './file-google.setting';
 import { IFileManager } from './file-manager.interface';
+const {Storage} = require('@google-cloud/storage');
+
 export class FileManagerGoogleService implements IFileManager{
+
+    private storage : Storage
+    private bucket 
+    constructor(){
+       this.storage = new Storage({
+            projectId : 'wasiatku',
+            keyFilename : 'wasiatku-89063501355a.json',
+        })
+        this.bucket  =  this.storage.bucket('keep_my_space')
+
+    }
+
+
   
     delete(filename: string): Promise<void> {
-        return bucketGoogle.file(filename).delete()
+        return this.bucket.file(filename).delete()
     }
 
     async signUrl(filename:string) : Promise<string>{
         try{
-            options.expires = Date.now() + 1  * 60 * 1000;
             
-        return bucketGoogle.file(filename).getSignedUrl(options)
+        return this.bucket.file(filename).getSignedUrl( {
+            version: 'v4',
+            action: 'read',
+            expires: Date.now() + 1  * 60 * 1000,
+          }
+        )
         }catch(error){
             console.log(error)
         }
     }
+
     
 }
--- src/core/file-manager/file-manager.service.ts
+++ src/core/file-manager/file-manager.service.ts
@@ -1,4 +1,3 @@
-import { bucketGoogle, expires, options } from './file-google.setting';
 import { FileManagerGoogleService } from './file-manager-google.service';
 import { IFileManager } from './file-manager.interface';
 
