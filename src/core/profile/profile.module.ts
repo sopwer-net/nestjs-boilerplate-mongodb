9e3ee50d058c5e0b0dd2beab339ee8d9cb50e99a
budazimbud <budazimbud@gmail.com>
2022-01-27 09:16:38 +0700

    register profile entity and account entity to module

--- src/core/profile/profile.module.ts
+++ src/core/profile/profile.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
+import { MongooseModule, Schema } from '@nestjs/mongoose';
+import { Profile, ProfileSchema } from './entities/profile.entity';
+import { Account, AccountSchema } from './entities/account.entity';
 
 @Module({
+  imports :[
+    MongooseModule.forFeature([{ name: Account.name, schema: AccountSchema }]),
+    MongooseModule.forFeature([{ name: Profile.name, schema: ProfileSchema }]),
+  ],
   controllers: [ProfileController],
   providers: [ProfileService]
 })
