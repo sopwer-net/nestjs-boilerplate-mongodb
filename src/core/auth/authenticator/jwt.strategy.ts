066abf51a4d0a04910a1c8441882e9c13ea219ea
budazimbud <budazimbud@gmail.com>
2022-01-29 11:29:22 +0700

    update payload jwt

--- src/core/auth/authenticator/jwt.strategy.ts
+++ src/core/auth/authenticator/jwt.strategy.ts
@@ -17,6 +17,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any) {
-    return { id: payload.sub, email: payload.email };
+    return { id: payload.id, email: payload.email };
   }
 }
