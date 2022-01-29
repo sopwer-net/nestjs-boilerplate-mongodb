8ddc2a1526d4b8c6e5468068586e7e5a5e6a117c
budazimbud <budazimbud@gmail.com>
2022-01-29 11:30:50 +0700

    overrider find one

--- src/core/profile/profile.repository.ts
+++ src/core/profile/profile.repository.ts
@@ -2,11 +2,19 @@ import { Injectable } from "@nestjs/common";
 import { InjectModel } from "@nestjs/mongoose";
 import { BaseRepository } from '../base-repository/base-repository.service';
 import { Profile, ProfileDocument } from './entities/profile.entity';
-import { Model } from 'mongoose';
+import { Model, FilterQuery } from 'mongoose';
 
 @Injectable()
 export class ProfileRepository extends BaseRepository<ProfileDocument>{
     constructor(@InjectModel(Profile.name) private profileModel : Model<ProfileDocument>){
         super(profileModel)
+    }
+
+    async findOne(
+        entityFilterQuery: FilterQuery<ProfileDocument>,
+        projection?: Record<string, unknown>
+    ): Promise<ProfileDocument | null>{
+
+        return this.entityModel.findOne(entityFilterQuery, {}).select('-hashedPassword')
     }
 }
